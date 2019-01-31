2902b30d3c2fa883ad4bbc2974c60fd6efe68f3f
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2019-01-31 23:43:15 +0100

    load configuration from env or file

--- dbdust/admin.py
+++ dbdust/admin.py
@@ -10,6 +10,7 @@
 """ Entry point of the dbdust application """
 
 import argparse
+import configparser
 import os
 
 
@@ -27,11 +28,51 @@ def create_cmd_line_parser():
     return parser
 
 
+class DbDustConfig(configparser.ConfigParser):
+    """ Handle configuration from environment variables or from a file
+
+    :param str configfile_path: path to the configfile or None
+    """
+    def __init__(self, configfile_path=None):
+        super(DbDustConfig, self).__init__()
+        self.read_dict(self._get_default_from_env())
+        if configfile_path:
+            self.read(configfile_path)
+
+    def _get_default_from_env(self):
+        """ Create a 2 levels dict from environment variables starting with `DBDUST___`
+        Used to setup default value in parser
+
+        `DBDUST___GENERAL__MY_VAR='value'` becomes `{'general': {'my_var': 'value'}}`
+
+        :return: dict built from environment variable
+        :type: dict
+        """
+        env = {k[9:]: v for (k, v) in os.environ.items() if k.startswith('DBDUST___')}
+        conf_from_env = {}
+        for k, v in env.items():
+            splited_key = k.split('__')
+            level1_key = splited_key[0].lower()
+            level2_key = splited_key[1].lower()
+            if level1_key not in conf_from_env:
+                conf_from_env[level1_key] = {}
+            conf_from_env[level1_key][level2_key] = v
+        return conf_from_env
+
+    def to_dict(self):
+        """ Helper to transform object to dict
+
+        :return: a dict version of the configparser
+        :rtype: dict
+        """
+        return {s: dict(self.items(s)) for s in self.sections()}
+
+
 def run(*args, **kwargs):
     """ Called by console_scripts `dbdust` to launch the workers
 
     Usage : `dbdust -c config.cfg`
     """
     args = create_cmd_line_parser().parse_args()
-
+    conf = DbDustConfig(args.config_file)
     print('dbdust running ...', args)
--- dbdust/test/test_admin.py
+++ dbdust/test/test_admin.py
@@ -20,3 +20,67 @@ def test_create_cmd_line_parser_with_config_file(tmpdir):
 
     assert args.config_file == str(tmp_config_file)
 
+
+def test_dbdustconfig_get_default_from_env_no_env(monkeypatch):
+    dbdust_conf = admin.DbDustConfig()
+    assert dbdust_conf._get_default_from_env() == {}
+
+
+def test_dbdustconfig_get_default_from_env_with_env(monkeypatch):
+    monkeypatch.setenv('RANDOM_ENV', 'value1')
+    monkeypatch.setenv('DBDUST___GENERAL__MY_VAR1', 'value1')
+    monkeypatch.setenv('DBDUST___GENERAL__MY_VAR2', 'value2')
+    monkeypatch.setenv('DBDUST___GENERAL__MY_VAR3', 'value3')
+    monkeypatch.setenv('DBDUST___SOURCE__OTHER_VAR1', 'test1')
+    monkeypatch.setenv('DBDUST___SOURCE__OTHER_VAR2', 'test2')
+    dbdust_conf = admin.DbDustConfig()
+
+    assert dbdust_conf._get_default_from_env() == {'general': {'my_var1': 'value1',
+                                                               'my_var2': 'value2',
+                                                               'my_var3': 'value3'},
+                                                   'source': {'other_var1': 'test1',
+                                                              'other_var2': 'test2'}}
+
+
+def test_dbdustconfig_from_file_no_env(tmpdir):
+    tmp_config_file = tmpdir.mkdir("dbdust").join("config.cfg")
+    tmp_config_file.write("""[general]
+    my_var1=value1
+    my_var2=value2
+    my_var3=value3
+    
+    [source]
+    other_var1=test1
+    other_var2=test2
+    """)
+
+    dbdust_conf = admin.DbDustConfig(str(tmp_config_file))
+    assert dbdust_conf.to_dict() == {'general': {'my_var1': 'value1',
+                                                 'my_var2': 'value2',
+                                                 'my_var3': 'value3'},
+                                     'source': {'other_var1': 'test1',
+                                                'other_var2': 'test2'}}
+
+
+def test_dbdustconfig_from_file_overwrite_env(monkeypatch, tmpdir):
+    monkeypatch.setenv('DBDUST___GENERAL__EXISTING_VAR', 'value')
+    monkeypatch.setenv('DBDUST___GENERAL__MY_VAR2', 'overwrite2')
+
+    tmp_config_file = tmpdir.mkdir("dbdust").join("config.cfg")
+    tmp_config_file.write("""[general]
+    my_var1=value1
+    my_var2=value2
+    my_var3=value3
+
+    [source]
+    other_var1=test1
+    other_var2=test2
+    """)
+
+    dbdust_conf = admin.DbDustConfig(str(tmp_config_file))
+    assert dbdust_conf.to_dict() == {'general': {'existing_var': 'value',
+                                                 'my_var1': 'value1',
+                                                 'my_var2': 'value2',
+                                                 'my_var3': 'value3'},
+                                     'source': {'other_var1': 'test1',
+                                                'other_var2': 'test2'}}
